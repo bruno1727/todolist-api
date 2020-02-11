e68644e361bed22f820125f81984a0eade5bc088
Bruno Morais <brunomoraisc@hotmail.com>
2020-02-10 23:22:51 -0300

    segundo commit na master

--- TaskManager/Startup.cs
+++ TaskManager/Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-//a
+//ad
 namespace TaskManager
 {
     public class Startup
