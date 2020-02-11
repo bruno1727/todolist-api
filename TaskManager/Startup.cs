1ff96bdfa14d912c65a2091acff7ed090efcd22e
Bruno Morais <brunomoraisc@hotmail.com>
2020-02-10 23:15:04 -0300

    teste4.1

--- TaskManager/Startup.cs
+++ TaskManager/Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-
+//teste4.1
 namespace TaskManager
 {
     public class Startup
