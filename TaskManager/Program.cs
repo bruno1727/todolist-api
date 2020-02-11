fe69b0c0ef8149e5f9824c1f0cfb1528974e9193
Bruno Morais <brunomoraisc@hotmail.com>
2020-02-11 00:41:05 -0300

    master conf

--- TaskManager/Program.cs
+++ TaskManager/Program.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 //aa
 namespace TaskManager
-{
+{//bb
     public class Program
     {
         public static void Main(string[] args)
