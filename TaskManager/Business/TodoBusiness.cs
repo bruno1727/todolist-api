f37b87fcd7240afef79209e79987c779148f1560
Bruno.Castro <bruno.castro@mag-it.com.br>
2020-08-10 22:44:16 -0300

    validacoa diferente de nulo ou vazio ao salvar e deletar

--- TaskManager/Business/TodoBusiness.cs
+++ TaskManager/Business/TodoBusiness.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using TodoList.Models;
 using TodoList.Requests;
@@ -17,6 +18,9 @@ namespace TodoList.Business
 
         public void AddTodo(IncludeTodoRequest request)
         {
+            if (request.Todos == null || !request.Todos.Any())
+                throw new Exception("Nenhum item informado");
+
             var todos = request.Todos.Select(t => new TodoModel { Description = t.Description });
             _context.Todo.AddRange(todos);
             _context.SaveChanges();
@@ -29,6 +33,9 @@ namespace TodoList.Business
 
         public IEnumerable<TodoModel> RemoveTodos(DeleteTodoRequest request)
         {
+            if (request.TodosIds == null || !request.TodosIds.Any())
+                throw new Exception("Nenhum item informado");
+
             var deleted = new List<TodoModel>();
             foreach(var id in request.TodosIds)
             {
